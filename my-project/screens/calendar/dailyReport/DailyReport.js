c057dd288e3719506e31fb267c3cbe15c38bd57d
kellyjung <jung55120@naver.com>
2022-04-26 14:37:38 +0900

    Feat: Connect with calendar Component

---
+++ my-project/screens/calendar/dailyReport/DailyReport.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ScrollView, Text } from "react-native";
+
+const DailyReport = () => {
+  return (
+    <ScrollView>
+      <Text>hello</Text>
+    </ScrollView>
+  );
+};
+
+export default DailyReport;
