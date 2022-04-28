29aed6a09ad674e20f9250701eeb9578e8b4f423
blue8959 <blue8959@naver.com>
2022-04-28 17:24:52 +0900

    ✨ Feat: Add Expert List
    전문가 목록 조회

---
+++ backend/src/main/java/com/neulbomi/neulbom/service/ConsultingServiceImpl.java
@@ -0,0 +1,43 @@
+package com.neulbomi.neulbom.service;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neulbomi.neulbom.entity.Expert;
+import com.neulbomi.neulbom.repository.ExpertRepository;
+
+@Service
+public class ConsultingServiceImpl implements ConsultingService {
+
+	@Autowired
+	private ExpertRepository expertRepository;
+	
+	@Override
+	public LinkedList<Object> getInfo() {
+		
+		List<Expert> experts = expertRepository.findByDelYnAndEnabledYnOrderByRegDtAsc("n", "y");
+		
+		LinkedList<Object> result = new LinkedList<>();
+		for (int i = 0; i < experts.size(); i++) {
+			Expert cur = experts.get(i);
+			
+			Map<String, Object> obj = new HashMap<>();
+			obj.put("expertName", cur.getExpertName());
+			obj.put("expertDesc", cur.getExpertDesc());
+			obj.put("expertImg", cur.getExpertImg());
+			
+			result.add(obj);
+		}
+		
+//		result.put("exper")
+		System.out.println(experts);
+		
+		return result;
+	}
+
+}
