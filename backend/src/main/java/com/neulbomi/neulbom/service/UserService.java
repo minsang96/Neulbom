82017a273ec50160c3c2d31d82b57ae0170f5b7a
yminsang96 <yminsang96@gmail.com>
2022-04-28 14:21:58 +0900

    ✨ Feat: Add Login API
    로그인 API 추가

--- backend/src/main/java/com/neulbomi/neulbom/service/UserService.java
+++ backend/src/main/java/com/neulbomi/neulbom/service/UserService.java
@@ -3,7 +3,6 @@ package com.neulbomi.neulbom.service;
 import com.neulbomi.neulbom.entity.User;
 
 public interface UserService {
-	
 	// 이메일로 user 찾기
 	public User getUserByEmail(String userEmail);
 }
