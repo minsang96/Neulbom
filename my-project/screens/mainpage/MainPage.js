81d3e86c3a405dce800bef281d83f5acf9967e0a
SSAFY <chance213213@gmail.com>
2022-05-03 16:33:25 +0900

    ✨ Feat: api 연결 useQuery랑 함수 생성해서 api 연결 client.js 사용해서 baseurl 이용

--- my-project/screens/mainpage/MainPage.js
+++ my-project/screens/mainpage/MainPage.js
@@ -1,16 +1,14 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { Dimensions, TouchableOpacity } from "react-native";
 import Ad from "./main/Ad";
 import DailyDiet from "./main/DailyDiet";
 import DietList from "./main/DietList";
 import styled from "styled-components/native";
-import Swiper from "react-native-web-swiper";
 import ButtonCompo from "../../components/button/ButtonCompo";
-import Stack from "../../navigation/Stack";
-import { ReactReduxContext, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import imagesSlice from "../../slices/images";
 import { useSelector } from "react-redux";
-import { Image, View, Text } from "react-native";
+import { getDiet } from "../../api/diets";
 
 const Box = styled.View`
   flex: 1;
@@ -27,10 +25,20 @@ const MainPage = ({ navigation: { navigate } }) => {
     console.log("testing");
   };
   const urls = useSelector((state) => state.images.imageurls);
-  const reduxIn = () => {
-    console.log(urls);
-    urls.map((url) => console.log(url.imageurls));
-  };
+  const reduxIn = useCallback(async () => {
+    // console.log(urls);
+    // urls.map((url) => console.log(url.imageurls));
+    console.log("ip");
+    try {
+      const response = await getDiet("2022-04-26", "1");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log("diet/daily");
+    }
+  });
+
   return (
     <Container style={{ backgroundColor: "white" }}>
       <Box>
@@ -40,9 +48,7 @@ const MainPage = ({ navigation: { navigate } }) => {
           onPressButton={() => navigate("Stack", { screen: "FoodWrite" })}
           buttonName="+ 혈당 추가"
         ></ButtonCompo>
-        {urls.map((url) => (
-          <Text key={url.id}>{url.imageurls}</Text>
-        ))}
+
         <ButtonCompo
           onPressButton={reduxTest}
           buttonName="redux test"
