dc9d8e2d2c7b71910e55ce2437af8edfd14b1017
kellyjung <jung55120@naver.com>
2022-05-04 14:37:31 +0900

    ✨Feat: add Consultant Mypage

--- my-project/screens/mypage/Mypage.js
+++ my-project/screens/mypage/Mypage.js
@@ -1,19 +1,52 @@
-import react from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
-
+import React, { useState } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import UserMypage from "./userMypage/UserMypage";
+import UserMypageUpdate from "./userMypageUpdate/UserMypageUpdate";
+import ConsultantMypage from "./consultantMypage/ConsultantMypage";
+import ConsultantMypageUpdate from "./consultantMypageUpdate/ConsultantMypageUpdate";
 
-const Mypage = () => (
-  <ScrollView style={styles.background}>
-    <UserMypage></UserMypage>
-  </ScrollView>
-);
+const NativeStack = createNativeStackNavigator();
+
+const Mypage = () => {
+  const [update, setUpdate] = useState(false);
+  const onClick = () => {
+    setUpdate(!update);
+    console.log(update);
+  };
+
+  return (
+    <NativeStack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerShown: false,
+      }}
+    >
+      {/* 일반 회원 */}
+      {/* <NativeStack.Screen name="Mypage">
+        {(props) => <UserMypage {...props} onClick={onClick} update={update} />}
+      </NativeStack.Screen>
+      <NativeStack.Screen name="MypageUpdate">
+        {(props) => (
+          <UserMypageUpdate {...props} onClick={onClick} update={update} />
+        )}
+      </NativeStack.Screen> */}
+      {/* 전문가 회원 */}
+      <NativeStack.Screen name="ConsultantMypage">
+        {(props) => (
+          <ConsultantMypage {...props} onClick={onClick} update={update} />
+        )}
+      </NativeStack.Screen>
+      <NativeStack.Screen name="ConsultantMypageUpdate">
+        {(props) => (
+          <ConsultantMypageUpdate
+            {...props}
+            onClick={onClick}
+            update={update}
+          />
+        )}
+      </NativeStack.Screen>
+    </NativeStack.Navigator>
+  );
+};
 
 export default Mypage;
-
-const styles = StyleSheet.create({
-  background: {
-    // backgroundColor: "white",
-    paddingHorizontal: 20,
-  },
-});
