d7aff45ffe36636f89db3b63b11fb797a80391e8
yminsang96 <yminsang96@gmail.com>
2022-05-04 14:00:41 +0900

    ✨ Feat: Add Ohter API
    술,커피,운동 기록 API 추가

---
+++ backend/src/main/java/com/neulbomi/neulbom/service/OtherService.java
@@ -0,0 +1,10 @@
+package com.neulbomi.neulbom.service;
+
+import com.neulbomi.neulbom.dto.OtherDto;
+
+public interface OtherService {
+	// 술,커피,운동 등록
+	public void addRecord(OtherDto otherDto);
+	// 술,커피,운동 기록 삭제
+	
+}
