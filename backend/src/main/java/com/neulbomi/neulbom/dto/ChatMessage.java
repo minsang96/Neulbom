1a61183482067e45188b9389d5b03a45e1722426
blue8959 <blue8959@naver.com>
2022-05-04 11:22:29 +0900

    ✨ Feat: Add chat
    채팅

--- backend/src/main/java/com/neulbomi/neulbom/dto/ChatMessage.java
+++ backend/src/main/java/com/neulbomi/neulbom/dto/ChatMessage.java
@@ -12,12 +12,11 @@ public class ChatMessage {
     }
 
     @Builder
-    public ChatMessage(MessageType type, String roomId, String sender, String message, long userCount) {
+    public ChatMessage(MessageType type, String roomId, int senderSeq, String message) {
         this.type = type;
         this.roomId = roomId;
-        this.sender = sender;
+        this.senderSeq = senderSeq;
         this.message = message;
-        this.userCount = userCount;
     }
 
     // 메시지 타입 : 입장, 퇴장, 채팅
@@ -27,7 +26,6 @@ public class ChatMessage {
 
     private MessageType type; // 메시지 타입
     private String roomId; // 방번호
-    private String sender; // 메시지 보낸사람
+    private int senderSeq; // 메시지 보낸사람
     private String message; // 메시지
-    private long userCount; // 채팅방 인원수, 채팅방 내에서 메시지가 전달될때 인원수 갱신시 사용
 }
