c58506e179629bce4b53e97ca435468ff6967cbc
sdi1358_ <sdi1358_@naver.com>
2022-05-06 23:05:20 +0900

    📝 Chore: Remove Unused File 사용하지 않는 파일 정리

--- backend/src/main/java/com/neulbomi/neulbom/dto/ChatMessageDto.java
+++ backend/src/main/java/com/neulbomi/neulbom/dto/ChatMessageDto.java
@@ -1,12 +1,31 @@
 package com.neulbomi.neulbom.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ChatMessageDto {
-	private String roomId;
-	private String writer;
-	private String message;
+
+    public ChatMessageDto() {
+    }
+
+    @Builder
+    public ChatMessageDto(MessageType type, String roomId, int senderSeq, String message) {
+        this.type = type;
+        this.roomId = roomId;
+        this.senderSeq = senderSeq;
+        this.message = message;
+    }
+
+    // 메시지 타입 : 입장, 퇴장, 채팅
+    public enum MessageType {
+        ENTER, QUIT, TALK
+    }
+
+    private MessageType type; // 메시지 타입
+    private String roomId; // 방번호
+    private int senderSeq; // 메시지 보낸사람
+    private String message; // 메시지
 }
