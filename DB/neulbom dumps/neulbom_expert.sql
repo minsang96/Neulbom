c1b79018837c6f13d4170d7a0befab4dcd5286bd
blue8959 <blue8959@naver.com>
2022-05-12 17:53:25 +0900

    📝 Chore: Update DB file
    Dump 파일 수정

--- DB/neulbom dumps/neulbom_expert.sql
+++ DB/neulbom dumps/neulbom_expert.sql
@@ -2,7 +2,7 @@
 --
 -- Host: k6a104.p.ssafy.io    Database: neulbom
 -- ------------------------------------------------------
--- Server version	8.0.28-0ubuntu0.20.04.3
+-- Server version	8.0.29-0ubuntu0.20.04.3
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,9 +26,9 @@ CREATE TABLE `expert` (
   `expert_seq` int NOT NULL AUTO_INCREMENT,
   `user_seq` int NOT NULL,
   `expert_name` varchar(10) NOT NULL,
-  `expert_img` varchar(100) NOT NULL,
+  `expert_img` varchar(200) NOT NULL,
   `expert_desc` varchar(100) NOT NULL,
-  `expert_cert` varchar(30) NOT NULL,
+  `expert_cert` varchar(30) DEFAULT NULL,
   `enabled_yn` varchar(1) NOT NULL,
   `del_yn` varchar(1) NOT NULL DEFAULT 'n',
   `reg_email` varchar(30) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE `expert` (
   UNIQUE KEY `expert_seq_UNIQUE` (`expert_seq`),
   KEY `expert_user_fk_idx` (`user_seq`),
   CONSTRAINT `expert_user_fk` FOREIGN KEY (`user_seq`) REFERENCES `user` (`user_seq`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +48,7 @@ CREATE TABLE `expert` (
 
 LOCK TABLES `expert` WRITE;
 /*!40000 ALTER TABLE `expert` DISABLE KEYS */;
-INSERT INTO `expert` VALUES (1,3,'박전문가','tkwls','경력 많은 의사','의사 자격증','y','n','expert@gmail.com','2022-04-26 01:43:28','expert@gmail.com','2022-04-26 01:43:28'),(2,4,'김선생님','kim','경력 많은 보건소 선생님','보건 자격증','y','n','expert2@gmail.com','2022-04-26 01:43:28','expert2@gmail.com','2022-04-26 01:43:28');
+INSERT INTO `expert` VALUES (1,3,'박전문가','tkwls','경력 많은 의사','의사 자격증','y','n','expert@gmail.com','2022-04-26 01:43:28','expert@gmail.com','2022-04-26 01:43:28'),(2,4,'김선생님','kim','경력 많은 보건소 선생님','보건 자격증','y','n','expert2@gmail.com','2022-04-26 01:43:28','expert2@gmail.com','2022-04-26 01:43:28'),(3,7,'string','string','string',NULL,'n','n','string','2022-04-27 11:23:26','string','2022-04-27 11:23:26'),(4,15,'허은아','https://neulbom-s3-bucket.s3.ap-northeast-2.amazonaws.com/Diet/1_profile_1651197572906.jpg','건강한 식습관 만들어요.',NULL,'n','n','sdi1358_@naver.com','2022-04-27 15:09:05','sdi1358_@naver.com','2022-04-27 15:09:05'),(5,17,'김의사','profile.jpg','건강한 식습관 만들어요.',NULL,'n','n','freessafy104@gmail.com','2022-05-04 15:36:03','freessafy104@gmail.com','2022-05-04 15:36:03');
 /*!40000 ALTER TABLE `expert` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -61,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-26 17:58:56
+-- Dump completed on 2022-05-12 17:47:47
