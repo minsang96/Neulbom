a7ac957e880886e88041decef2c365259240ac97
SSAFY <chance213213@gmail.com>
2022-05-17 23:29:43 +0900

    ✨ Feat: show food info

--- my-project/slices/images.js
+++ my-project/slices/images.js
@@ -27,6 +27,7 @@ const imagesSlice = createSlice({
       state.dinner = action.payload.dinner.dietList;
       state.total_dinner = action.payload.dinner.total;
     },
+
     add: (state, action) => {
       state.add.push({ food: action.payload.tempFood, id: nextId });
       state[`total_${action.payload.current}`] = {
@@ -53,6 +54,7 @@ const imagesSlice = createSlice({
       //   ],
       // };
     },
+
     addS3url: (state, action) => {
       return {
         ...state,
@@ -63,6 +65,7 @@ const imagesSlice = createSlice({
         ],
       };
     },
+
     addImageUrls: (state, action) => {
       state.imageurls.push({ imageurl: action.payload, id: nextId_image });
       nextId_image += 1;
@@ -93,11 +96,10 @@ const imagesSlice = createSlice({
       // if (action.payload.current == "breakfast") {
       return {
         ...state,
-        breakfast: [
-          state.breakfast.filter(
-            (food) => food.dietSeq !== action.payload.dietSeq
-          ),
-        ],
+        breakfast: state.breakfast.filter(
+          (food) => food.dietSeq !== action.payload.dietSeq
+        ),
+
         // };
       };
       //   } else if (action.payload.current == "lunch") {
