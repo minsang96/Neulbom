d94e0b9b84ce7bd54775ddcf0a75ed447d4e10db
sdi1358_ <sdi1358_@naver.com>
2022-05-18 01:28:21 +0900

    ✨ Feat: Add WebSocket user subscribe WebSocket 로직 변경 초안

--- backend/src/main/java/com/neulbomi/neulbom/controller/ChatController.java
+++ backend/src/main/java/com/neulbomi/neulbom/controller/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 import com.neulbomi.neulbom.dto.ChatMessageDto;
+import com.neulbomi.neulbom.dto.SocketUserDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,5 +22,14 @@ public class ChatController {
 //			message.setMessage(message.getSender() + "님이 입장하셨습니다.");
 		messagingTemplate.convertAndSend("/api/sub/chat/room/" + message.getRoomId(), message);
 
+	}
+    
+    // User가 로그인하면, /api/sub/user/{userSeq} 구독하기
+    @MessageMapping("/user")
+	public void connect(SocketUserDto socketUser) {
+    	// A가 방 생성하면 방 생성하고, B에게 생성 알림 가게
+    	// B에서 알림 받고 방 구독하도록
+		if (SocketUserDto.SocketType.CREATE.equals(socketUser.getType())) 
+			messagingTemplate.convertAndSend("/api/sub/user/" + socketUser.getRecvSeq(), socketUser);
 	}
 }
