d48edff61594e1eb0904e7365e70dbd11f9797e2
sdi1358_ <sdi1358_@naver.com>
2022-05-19 15:17:13 +0900

    🎨 Style: Change Neulbom Notice 늘봄 공지사항 CSS

--- my-project/components/infoBox/noticeCompo/Notice2.js
+++ my-project/components/infoBox/noticeCompo/Notice2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
-import { Dimensions } from "react-native";
+import { Dimensions, Image } from "react-native";
 
 const screenSize = Dimensions.get("screen");
 
@@ -14,20 +14,59 @@ const Notice2 = () => {
         <Text style={{ textAlign: "right" }}>2022-05-14</Text>
 
         <View style={styles.line} />
-        <Text>
-          #간식도 기록하자!{"\n"}
-          (간식 검색 페이지){"\n"}
-          간식도 식단에 기록하고 싶으셨다구요? 그래서 준비했습니다!{"\n"}
-          [그 검색하는 데이터들 출처]에서 제공하는 [몇개]의 음식 정보들을 통해
-          검색해서 기록할 수 있어요!{"\n"}
-          {"\n"}
-          #전문가 상담할 때 내 식단을 보고{"\n"}
-          (전문가가 상담받는 사람 식단 보는 페이지){"\n"}
-          이제는 상담할 때 말이 아니라 직접 내 식단 기록을 보고 상담할 수
-          있어요!{"\n"}
-          자료를 바탕으로 상담할 수 있으니 조금 더 상세하게 상담 받을 수 있겠죠?
-          {"\n"}
-        </Text>
+        <View style={styles.titleBox}>
+          <Text style={styles.contentTitle}>
+            🌱늘봄의 새로운 기능을 소개합니다!
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.subheading}>#1 간식도 기록하자!</Text>
+          <Text style={styles.subContent}>
+            간식도 식단에 기록하고 싶으셨다구요? 그래서 준비했습니다!{"\n"}
+            [그 검색하는 데이터들 출처]에서 제공하는 [몇 개]의 음식 정보들을
+            통해 검색해서 기록할 수 있어요!
+          </Text>
+          {/* <Image
+            source={require("../../assets/images/dailyReport2.PNG")}
+            style={{
+              width: screenSize.width * 0.78,
+              height: 380,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          ></Image>
+          <Image
+            source={require("../../assets/images/dailyReport2.PNG")}
+            style={{
+              width: screenSize.width * 0.78,
+              height: 200,
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: 30,
+            }}
+          ></Image> */}
+        </View>
+        <View>
+          <Text style={styles.subheading}>
+            # 2 전문가 상담할 때 내 식단을 보고
+          </Text>
+          <Text style={styles.subContent}>
+            이제는 상담할 때 말이 아니라 직접 내 식단 기록을 보고 상담할 수
+            있어요!{"\n"}
+            자료를 바탕으로 상담할 수 있으니 조금 더 상세하게 상담 받을 수
+            있겠죠?
+          </Text>
+          <Image
+            source={require("../../assets/images/mainpage1.jpeg")}
+            style={{
+              width: screenSize.width * 0.78,
+              height: 380,
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: 30,
+            }}
+          ></Image>
+        </View>
       </View>
     </ScrollView>
   );
@@ -60,5 +99,22 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
+  },
+  contentTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  subheading: {
+    fontSize: 17,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  subContent: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginLeft: 5,
   },
 });
